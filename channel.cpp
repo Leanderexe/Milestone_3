25caa7eb502411cbe578025113dc3369ec0edc91
Leanderexe <59029811+Leanderexe@users.noreply.github.com>
2020-02-04 18:00:51 +0100

    Add files via upload

--- channel.cpp
+++ channel.cpp
@@ -7,16 +7,27 @@
 #include "time.h"
 #include <map>
 
-Channel::Channel(int number_channel,std::string object_name){
+Channel::Channel(int number_channel,std::string object_name, int rows, int columns){
     this->num_channel = number_channel;
     this->object_name = object_name;
+    this->columns = columns;
+    this->rows = rows;
 }
+
+void Channel::set_numcols(int columns){
+    this->columns = columns;
+}
+
+void Channel::set_numrows(int rows){
+    this->rows = rows;
+}
+
 
 void Channel::LReLU(){
  for(int i = 0; i < (this->rows); i++)
- for(int z = 0; z < (this->column);z++){
-    if (kernel[i][z] < 0){
-        kernel[i][z] = kernel[i][z] * 0.01;
+ for(int z = 0; z < (this->columns);z++){
+    if (kernel->getValue(i, z) < 0){
+        kernel->setValue(i,z, kernel->getValue(i, z) * 0.01);
      }
    else{
 }
@@ -25,8 +36,8 @@ void Channel::LReLU(){
 
  void Channel::Print(){
      for(int i = 0; i < (rows); i++){
-     for(int z = 0; z < (column);z++){
-         cout << this->kernel[i][z] << ' ';
+     for(int z = 0; z < (columns);z++){
+         cout << kernel->getValue(i, z) << ' ';
  }
      cout << endl;
      }
@@ -35,27 +46,29 @@ void Channel::LReLU(){
 
 void Channel::Get_rc(int row, int column){
     this->rows = row;
-    this->column = column;
+    this->columns = column;
 }
 
-void Channel::Get_Kernel(vector<vector<double>> kernel){
+// Get_Kernel, ist glaube ich überflüssig wenn man mit pointer arbeitet.
+void Channel::Get_Kernel(vector<vector<double>> kern){  // Pushes a given matrix into the kernel.
     vector <double> temp;
     for(int i = 0; i < (rows); i++){
-    for(int z = 0; z < (column);z++){
-        temp.push_back(kernel[i][z]);
-}
-this->kernel.push_back(temp);
-temp.clear();
+    for(int z = 0; z < (columns);z++){
+    kernel->setValue(i,z,kern[i][z]);
+    }
 }
     }
 
+// keine ahnung was die macht bitte anpassen/verpassen
+// gleiche gilt für die unteren funktionen
 
-vector<vector<double>> Channel::Getter_Kernel(){
+
+vector<vector<double>> Channel::Getter_Kernel(){ // Returns the matrix of the kernel. Use it to get the initialized random Matrix from class Kernel.
     vector <vector <double>> help;
     vector <double> temp;
     for(int i = 0; i < (rows); i++){
-    for(int z = 0; z < (column);z++){
-        temp.push_back(this->kernel[i][z]);
+    for(int z = 0; z < (columns);z++){
+        temp.push_back(kernel->getValue(i, z));
     }
     help.push_back(temp);
     temp.clear();
@@ -64,31 +77,29 @@ vector<vector<double>> Channel::Getter_Kernel(){
 }
 
 void Channel::Clear(){
-    vector <vector<double>> vector;
-    vector.swap(this->kernel);
+    matrix* vector = new matrix(0, 0, false);
+    this->kernel = vector;
     this->rows = 0;
-    this->column = 0;
+    this->columns = 0;
     // this->kernel.shrink_to_fit();
 }
 
 
 
-void Channel::Set(vector<vector<double>> input){
-    this->kernel.swap(input);
+void Channel::Set(matrix* input){
+    this->kernel = input;
 }
 
 
-void Channel::Add(vector<vector<double> > input){
+void Channel::Add(vector<vector<double>> input){  // Adds a given input to the allready existing kernel.
     vector<double> temp;
     for(int i = 0; i < (input.size()); i++){
     for(int z = 0; z < (input[0].size());z++){
-        temp.push_back(input[i][z]);
+        kernel->setValue(i,z,input[i][z]);
 }
-this->kernel.push_back(temp);
-temp.clear();
   }
     this->rows += input.size();  // Adds the number of added rows to the attribute.
-    this->column = input[0].size();
+    this->columns = input[0].size();
    }
 
 int Channel::Getter_Rows(){
@@ -96,5 +107,5 @@ int Channel::Getter_Rows(){
 }
 
 int Channel::Getter_Column(){
-    return(this->column);
+    return(this->columns);
 }
--- channel.h
+++ channel.h
@@ -2,14 +2,22 @@
 #define CHANNEL_H
 #include <string>
 #include <kernel.h>
+#include "matrix.h"
 
 
 class Channel
 {
 public:
-    Channel(int num_channel, std::string object_name);
-    void Set(vector<vector<double>> input);   // Sets a new Kernel.
-    void Add(vector<vector<double>> input);   // Adds something to a allready existing Kernel.
+    Channel(int num_channel, std::string object_name, int rows, int columns);
+    void Set(matrix* input);   // Sets a new Kernel.
+    void Add(vector<vector<double>> input);
+    // Adds something to a allready existing Kernel.
+
+    void set_numrows(int rows);
+    void set_numcols(int columns);
+
+
+
     void Clear();
     void LReLU();
     void Get_rc(int row, int column);
@@ -23,8 +31,10 @@ public:
     int num_channel;
     std::string object_name;
     int rows;
-    int column;
-    vector<vector<double>> kernel;
+    int columns;
+    matrix*  kernel;
+
+    //replaced vector<vector<double>> with matrix* objects
 
 };
 
